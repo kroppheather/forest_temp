9bdbf57ecc1268db6f169a25d2b00779b5f46292
kroppheather <kroppheather@gmail.com>
2025-08-05 11:51:09 -0400

    data vis to get an idea of range and statistical assumptions

--- Analysis_figures.R
+++ Analysis_figures.R
@@ -34,7 +34,7 @@ soilAll <- left_join(tomstDay, airDay, by=c("year","doy"))
 soilAll$TempDiff <- soilAll$TSoil6 - soilAll$aveT 
 soilAll$date <- as.Date(soilAll$doy-1, origin=paste0(soilAll$year, "-01-01"))
 
-soilAll$VWC_f <- ifelse(soilAll$TSoil6)
+soilAll$VWC_f <- ifelse(soilAll$TSoil6 <0,NA, soilAll$VWC)
 
 ggplot(soilAll, aes(date, TSoil6, color=location))+
   geom_line(alpha=0.5)
@@ -53,6 +53,47 @@ weatherDay$date <- as.Date(weatherDay$doy-1, origin=paste0(weatherDay$year, "-01
 
 ggplot(weatherDay, aes(date, SNWD/10))+
   geom_col(alpha=0.5)
+
+
+ggplot(soilAll, aes(VWC_f, TSoil6, color=location))+
+  geom_point(alpha=0.5)
+
+soilmet <- left_join(soilAll, weatherDay, by=c("doy","year"))
+
+ggplot(soilAll, aes(Tsurf15, TSoil6, color=location))+
+  geom_point(alpha=0.5)
+ggplot(soilAll, aes(aveT, TSoil6, color=location))+
+  geom_point(alpha=0.5)
+
+ggplot(soilAll%>%filter(location=="Buckthorn RG03"), aes(aveT, TSoil6, color=VWC_f))+
+  geom_point()
+ggplot(soilAll%>%filter(location=="Rogers reforestation"), aes(aveT, TSoil6, color=VWC_f))+
+  geom_point()
+ggplot(soilAll%>%filter(location=="Spruce RG09"), aes(aveT, TSoil6, color=VWC_f))+
+  geom_point()
+ggplot(soilAll%>%filter(location=="hemlock sapflow"), aes(aveT, TSoil6, color=VWC_f))+
+  geom_point()
+ggplot(soilAll%>%filter(location=="maple-beech"), aes(aveT, TSoil6, color=VWC_f))+
+  geom_point()
+
+ggplot(soilmet%>%filter(location=="Spruce RG09"&aveT.x<0), aes(aveT.x, TSoil6, color=SNWD))+
+  geom_point()
+ggplot(soilmet%>%filter(location=="Buckthorn RG03"&aveT.x<0), aes(aveT.x, TSoil6, color=VWC_f))+
+  geom_point()
+ggplot(soilmet%>%filter(location=="Buckthorn RG03"&aveT.x<0), aes(VWC_f, TSoil6))+
+  geom_point()
+ggplot(soilmet%>%filter(location=="Spruce RG09"&aveT.x<0), aes(VWC_f, TSoil6))+
+  geom_point()
+
+ggplot(soilmet%>%filter(location=="maple-beech"&aveT.x<0), aes(VWC_f, TSoil6))+
+  geom_point()
+
+ggplot(soilmet%>%filter(location=="maple-beech"&aveT.x<0), aes(aveT.x, TSoil6))+
+  geom_point()
+
+ggplot(soilmet, aes(SNWD, TSurf2, color=location))+
+  geom_point(alpha=0.5)
+
 #### basic time series plots to visualize data
 
 wd <- 20
